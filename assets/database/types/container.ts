e93705a930c0a13ea9f0e87eb34c194c2356c266
NotPlai <suphakornkhamwongsa60@gmail.com>
2025-07-10 10:03:13 +0700

    Update container.ts

--- assets/database/types/container.ts
+++ assets/database/types/container.ts
@@ -6,22 +6,3 @@ export interface ClientCredentialsContainer {
     "client-secret": string
     "token": string
 }
-
-// Members Level & Members Level Labels
-export interface MembersLevelContainer {
-    "username": string
-    "user-id": string
-    "permission-level": number
-}
-
-export interface MembersLLabelContainer {
-    "name": string
-    "permission-level": number
-}
-
-// Others API Credentials Build!
-export interface OpenAICredentials {
-    "project": string,
-    "organization": string,
-    "authorization": string
-}
