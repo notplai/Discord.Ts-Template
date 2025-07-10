b7c4bd020ce5ccf6fa7d3d9d01fa850b9d4705d4
NotPlai <suphakornkhamwongsa60@gmail.com>
2025-07-10 10:03:32 +0700

    Update credentials.sql

--- assets/database/builders/credentials.sql
+++ assets/database/builders/credentials.sql
@@ -8,26 +8,3 @@ CREATE TABLE "client" (
     "client-secret": TEXT NOT NULL UNIQUE,
     PRIMARY KEY('name', 'token')
 )
-
-CREATE TABLE "members-level" (
-    "name": TEXT NOT NULL UNIQUE,
-    "user-id": TEXT NOT NULL UNIQUE,
-    "permission-level": INT NOT NULL,
-    PRIMARY KEY('name')
-)
-
-CREATE TABLE "permission-rlink" (
-    "name": TEXT NOT NULL UNIQUE,
-    "permission-level" INT NOT NULL UNIQUE,
-
-    PRIMARY KEY('permission-level')
-)
-
-CREATE TABLE "OpenAI" (
-    "name": TEXT NOT NULL UNIQUE,
-    "project": TEXT NOT NULL UNIQUE,
-    "organization": TEXT NOT NULL UNIQUE,
-    "authorization": TEXT NOT NULL UNIQUE
-
-    PRIMARY KEY('name')
-)
